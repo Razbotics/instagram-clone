d7368f0179ce51e1c102615843cc232af5c2ab98
Shubhankar <das.shubhankar97@gmail.com>
2021-03-09 02:11:15 +0530

    configured firebase

--- app/config/firebase.js
+++ app/config/firebase.js
@@ -1,6 +1,8 @@
 import { API_KEY } from "@env";
+import firebaseLib from "firebase/app";
+import "firebase/firestore";
 
-export default {
+const firebaseConfig = {
   apiKey: API_KEY,
   authDomain: "instagram-clone-771e3.firebaseapp.com",
   projectId: "instagram-clone-771e3",
@@ -8,3 +10,9 @@ export default {
   messagingSenderId: "784311896120",
   appId: "1:784311896120:web:1e6edbc32ea52c3aeffecd",
 };
+
+if (!firebaseLib.apps.length) firebaseLib.initializeApp(firebaseConfig);
+else firebaseLib.app();
+
+export const firebase = firebaseLib;
+export const db = firebaseLib.firestore();
